146144007826b13ae1267ad6023dac314403d103
Xavier Roma <xavier.roma.2015@salleurl.edu>
2018-02-07 17:37:49 +0100

    funcionalitat inutil

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code there
-        System.out.println("hello world");
+        System.out.println(funcioinutil());
+    }
+
+    public static String funcioinutil(){
+        return "Hello World";
     }
 }
